52dfa0372ac22c86b47b2ce5b71d4adc3fa52d21
Khaydarin <khaydarin@debian.debian_network>
2016-04-24 20:52:54 +0200

    .

--- main.cpp
+++ main.cpp
@@ -1,23 +1,60 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <cstdlib>
+#include <ncurses.h>
 
-char **charSquare(int n);
-void drawCharSquare(char **square, int n);
+void fibonacci(int wyrazy);
+void geometric2(int wyrazy);
 
-int main(int argc, char **argv)
+
+
+
+int main(int argc, char** argv)
 {
-	int n;
-	drawCharSquare(charSquare(n), n);	
+
+	char wybor = *(argv[2]);
+	int wyrazy = atoi(argv[1]);
+
+	if(wybor=='f')
+	{
+		fibonacci(wyrazy);
+	}
+	else if(wybor=='g')
+	{
+		geometric2(wyrazy);
+	}
+	else
+	{
+		printf("Nieznany parametr");
+	}
+
+	printf("\n\n");
+
+	getch();
+
 }
 
-char **charSquare(int n)
+void fibonacci(int wyrazy)
 {
-	char **square;
-	return square;
+
+	unsigned int w1 =0;
+	unsigned int w2 =1;
+
+	for(int i=0;i<wyrazy;i++)
+	{
+		printf("%d ", w2);
+
+		w2 = w1 + w2;
+		w1 = w2 - w1;
+	}
+
 }
 
-void drawCharSquare(char **square, int n)
+void geometric2(int wyrazy)
 {
-
+	unsigned int w1 = 1;
+	for(int i=0;i<wyrazy;i++)
+	{
+		printf("%d ", w1);
+		w1*=2;
+	}
 }
